c2319d48d61b69562565087ec769558519473101
Dikshya BC <dikshya.bc@featherwebs.com>
2021-07-06 14:31:20 +0545

    primary

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
-import React from 'react'
-
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import Sidebar from './components/Sidebar';
+import Login from './components/Login';
+import { BrowserRouter as Router } from 'react-router-dom';
 const App = () => {
   return (
-    <div>
-      
+  <Router>
+    <div className="App">
+    <Login />
     </div>
+  </Router>
   )
 }
 
---
+++ frontend/src/components/Login.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+	return (
+		<div>
+			<Row className="login-page">
+
+				{/* left-side */}
+				<Col md={6} className="left">
+					<img src="images/feather-login-img.png" alt=""></img>
+				</Col>
+
+
+				{/* right-side */}
+				<Col md={6} className=" d-flex flex-all form right">
+					<div className="log-content">
+						<h1 className="text-center feather">FeatherWebs </h1>
+						<div className="log-body">
+							<div className="form-group myr-top mb-4">
+								<label>Email</label>
+								<input type="text" className="form-control custom" placeholder="Someone123@gmail.com" required ></input>
+							</div>
+							<div className="form-group myr-top mb-4">
+								<label>Password</label>
+								<input type="password" className="form-control custom" placeholder="********" required></input>
+							</div>
+							<div class="form-check d-flex justify-content-between mb-5">
+
+								<label className="form-check-label" for="exampleCheck1"><input type="checkbox" className="form-check-input" id="exampleCheck1"></input>Keep me logged In</label>
+								<Link to="/">Forgot Password?</Link>
+							</div>
+							<div className="log-btn text-center">
+								<Link to="#" className="btn btn-primary">Sign In</Link>
+							</div>
+
+						</div>
+					</div>
+				</Col>
+			</Row>
+		</div>
+	)
+}
+
+export default Login
