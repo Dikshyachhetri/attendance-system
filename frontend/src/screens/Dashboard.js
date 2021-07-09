f7589fed4af481366fe15f3637cbc931e7d8b941
simron6917 <simron.shrestha@featherwebs.com>
2021-07-09 18:47:30 +0545

    problem in calendar

--- frontend/src/screens/Dashboard.js
+++ frontend/src/screens/Dashboard.js
@@ -1,30 +1,29 @@
-import React from 'react'
-import Sidebar from '../components/Sidebar'
-import { Row, Col } from 'react-bootstrap'
-import "../css/login.cssgit "
-import DatePicker from "react-datepicker";
-
+import React from "react";
+import Sidebar from "../components/Sidebar";
+import { Row, Col } from "react-bootstrap";
+import "../css/login.css";
+import "../calendar.css";
 
 const Dashboard = () => {
-    
-    return (
-        
-        <>
-        <Row>
+  return (
+    <>
+      <Row>
         <Col lg={2}>
-        <Sidebar />
+          <Sidebar />
         </Col>
         <Col lg={10}>
-        <Row>
+          <Row>
             <Col lg={6}>
-                <h1 className="heading">Dashboard</h1>
-                
+              <h1 className="heading">Dashboard</h1>
+              <div className="calendar"></div>
+<script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.5.1/moment.min.js"></script>
             </Col>
-        </Row>
+          </Row>
         </Col>
-        </Row>
-        </>
-    )
-}
+      </Row>
+      
+    </>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
